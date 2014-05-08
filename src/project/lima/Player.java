dc14b35d00c01c9cbb5198733f23b720beb11a00
Eric <Eric@Eric-HP>
2014-05-08 18:25:58 -0500

    add exceptions, add stuff to player class

--- src/project/lima/Player.java
+++ src/project/lima/Player.java
@@ -4,8 +4,10 @@
  */
 package project.lima;
 
+import java.util.ArrayList;
 import project.deck.DeckofCards;
 import project.card.Card;
+import project.support.*;
 
 /**
  *
@@ -15,7 +17,85 @@ public class Player {
 	
 	private String name;
 	private DeckofCards deck;
-	private Card[] hand;
-	private int health;
+	private ArrayList<Card> hand = new ArrayList<Card>(10);
+	private int currentCredits;
+	private final int maxCredits = 50;
+	private int wcp;
 	
+	public String getName() 
+	{
+		return this.name;
+	}
+	
+	public DeckofCards getDeck()
+	{
+		return this.deck;
+	}
+	
+	public ArrayList<Card> getHand()
+	{
+		return this.hand;		
+	}
+	
+	public int getCredits()
+	{
+		return this.currentCredits;
+	}
+	
+	public int getMaxCredits()
+	{
+		return this.maxCredits;
+	}
+		
+	public int getWcp()
+	{
+		return this.wcp;
+	}
+	
+	public void setCredits(int credit)
+	{
+		if(currentCredits + credit > maxCredits)
+			this.currentCredits = maxCredits;
+		else if (currentCredits + credit <= 0)
+			this.currentCredits = 0;
+		else
+			this.currentCredits += credit;
+	}
+	
+	public void setWcp(int wcp)
+	{
+		this.wcp = wcp;
+	}
+	
+	public void draw()
+	{
+		if (hand.size() < 10)
+			hand.add(deck.deal());
+	}
+	
+	public Card playCard(String name) throws RuntimeException
+	{
+		int length = hand.size();
+		for (int i = 0 ; i < length ; i++)
+		{
+			if (hand.get(i).getName().equals(name))
+			{
+				if (hand.get(i).getCost() <= wcp)
+				{
+					wcp -= hand.get(i).getCost();
+					Card temp = hand.get(i);
+					hand.remove(i);
+					return temp;
+				}
+				else
+					throw new MuchCostException("Card cost too much!");
+			}
+		}
+		throw new NotInHandException("Card is not in hand!");
+	}
+	
+	public void setCredits(int credit)
+	{
+		this.currentCredits = credit;
+	}	
 }
