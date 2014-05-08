55a71ba795fb0a7d8e998a2f68f3e8171e95fb2c
benjaminstrick <benjaminstrick@dhcp-10-101-134-20.wireless.northwestern.private>
2014-05-08 17:44:03 -0400

    add all methods to card

--- src/project/card/Card.java
+++ src/project/card/Card.java
@@ -9,18 +9,65 @@ import java.awt.image.BufferedImage;
  */
 public class Card {
 	
+    public enum ability{damage, rethand, friendbuff, heal;}
+    
 	private String name;
 	private int cost;
 	private BufferedImage image;
         private String description;
-	
-
-	public Card(String name, int cost, BufferedImage image, String d)
+	private ability ability;
+        
+        
+	public Card(String name, int cost, BufferedImage image, String d, ability a)
 	{
 		this.name = name;
 		this.cost = cost;
 		this.image = image;
                 this.description = d;
+                this.ability = a;
 	}
 	
+        // Getters and Setters
+        public String getName(){
+            return name;
+        }
+        
+        public void setName(String n){
+            this.name = n;  
+        }
+        
+        
+            public int getCost(){
+            return cost;
+        }
+        
+        public void setCost(int c){
+            this.cost = c;
+        }    
+         
+            public BufferedImage getImage(){
+            return image;
+        }
+        
+        public void setImage(BufferedImage i){
+            this.image = i;
+        }  
+        
+            public String getDesc(){
+            return description;
+        }
+        
+        public void setDesc(String d){
+            this.description = d;
+        } 
+        
+        public ability getAbility(){
+            return ability;
+        }
+        
+        public void setAbility(ability a){
+            this.ability = a;
+        }
 }
+        
+
