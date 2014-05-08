641c2b8e869a7848b3bc77bdac51bfb8b6e5cf31
benjaminstrick <benjaminstrick@dhcp-10-101-134-20.wireless.northwestern.private>
2014-05-08 17:19:25 -0400

    added description parameter to card

--- src/project/card/Card.java
+++ src/project/card/Card.java
@@ -12,13 +12,15 @@ public class Card {
 	private String name;
 	private int cost;
 	private BufferedImage image;
+        private String description;
 	
 
-	public Card(String name, int cost, BufferedImage image)
+	public Card(String name, int cost, BufferedImage image, String d)
 	{
 		this.name = name;
 		this.cost = cost;
 		this.image = image;
+                this.description = d;
 	}
 	
 }
