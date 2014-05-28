f637537db601fc7d214e30ff2bccf53ee63ce7c7
esun9064 <ericsun2016@u.northwestern.edu>
2014-05-28 02:05:32 -0500

    test playerCard p2p

--- src/project/thread/GameThread.java
+++ src/project/thread/GameThread.java
@@ -1,8 +1,8 @@
 /*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 package project.thread;
 import java.io.*;
@@ -127,6 +127,10 @@ public class GameThread extends Thread
 				}
 				
 			}
+			gamepad.updateBoardCards();
+			gamepad.updateHandCards();
+			gamepad.updatePlayerStats();
+			
 		}
 		else if(recMessage.startsWith("/yourname "))
 		{
